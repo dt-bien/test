77f73dbcf1c322a711c40db58c1946afc38d5c05
test <testemail>
2021-03-28 20:44:14 +0700

    Initial commit

---
+++ client.php
@@ -0,0 +1,4 @@
+<form name="form" method="post" action="server.php" enctype="multipart/form-data" >
+<input type="file" name="my_file" /><br /><br />
+<input type="submit" name="submit" value="Upload"/>
+</form>
